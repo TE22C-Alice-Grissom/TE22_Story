6cc9dedfb5902d99e8d1afb87f6cbd3cba81bdb1
Alice Grissom <Alice.grissom@elev.ga.ntig.se>
2023-09-25 08:39:03 +0200

    IAHSbföiASJBf

--- TE22_Story/Program.cs
+++ TE22_Story/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Vill du gå höger eller vänster?");
+string riktning = Console.ReadLine();
+//ToLower();
+
+
+if (riktning == "Höger" || riktning == "höger")
+{
+    Console.WriteLine("Du gick rätt. Du forsätter längs stigen och ser en run längst vägen. Ska du kolla ner eller inte?");
+    string check = Console.ReadLine();
+    if (check == "ja")
+    {
+        Console.WriteLine("Det var ett monster i brunnen. Du dog, HAHAHAHAHAHAHAH");
+    }
+    else if (check == "nej")
+    {
+
+        Console.WriteLine("Du kom dit du ville utan att dö, YIPPIE");
+    }
+}
+if (riktning == "Vänster" || riktning == "vänster")
+{
+    Console.WriteLine("Du dog din noob HAHHAHAHAHAH SKILL ISSUE KYS");
+}
+
+Console.ReadLine();
